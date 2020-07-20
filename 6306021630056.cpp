8e9521bf01087d9a32fbb3ad3bdac15c3c7c092a
Wuthinan-github <68538634+Wuthinan-github@users.noreply.github.com>
2020-07-20 16:28:32 +0700

    Add files via upload

---
+++ 6306021630056.cpp
@@ -0,0 +1,16 @@
+#include<iostream>			
+using namespace std;
+void main() {
+	cout << "  1st term of 2020		\\6306021630056   Wuthinan Tanomsap" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject Name				| Score(100) | Grade |" << endl; 
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "|   4570034  | Introduction to combuter	|     85     |  \"A\"" << endl;
+	cout << "|   4580247  | English					    |     74     |  \"B\"" << endl;
+	cout << "|   4570034  | Design Thinking				|     92     |  \"A\"" << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << "|    TOTAL   | Subject = 3				|  Average = 3.87" << endl;
+	cout << "=========================================================" << endl << endl;
+	cout << "KMUTNB" << endl;
+	system("pause");
+}
